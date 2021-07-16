dfc727be9f28ae33e66befcd6525a79f9eb5670f
Yosuke0831 <may.yosuke@ezweb.ne.jp>
2021-07-16 16:40:49 +0900

    3の倍数の発展星2

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,6 +1,17 @@
+// const genkiFunction = function(number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 3 === 0) {
+//       console.log(`${n}!!!!!!!`)
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+
 const genkiFunction = function(number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
+    m = JSON.stringify(n).split("")
+    if (m.includes("3")) {
       console.log(`${n}!!!!!!!`)
     } else {
       console.log(n)
